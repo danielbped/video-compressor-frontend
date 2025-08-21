302bf317069bc7c954c146049cd91fd409afd7d6
Daniel Batista <danielbped@gmail.com>
2025-08-21 11:29:28 -0300

    feat: setup redux

---
+++ src/redux/actionTypes/user.tsx
@@ -0,0 +1,9 @@
+import { type User } from "../../interfaces/user-data.interface"
+
+export type ActionUserType = {
+  type: string,
+  payload: User
+}
+
+export const GET_USER = 'GET_USER'
+export const LOGOUT = 'LOGOUT'
