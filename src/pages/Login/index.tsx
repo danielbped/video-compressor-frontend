f6675a74b035f6f5b7a544754d39cfce46cf5e35
Daniel Batista <danielbped@gmail.com>
2025-08-21 11:29:38 -0300

    feat: adds login page

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,76 @@
+import LoginForm from "../../components/LoginForm"
+import { StyledLogin } from "./style"
+import { type LoginData, type LoginResponse } from '../../interfaces/login-data.interface'
+import useLoginMutate from "../../hooks/useLoginMutate"
+import { useNavigate } from "react-router-dom"
+import { useLocalStorage } from "@uidotdev/usehooks"
+import { type ChangeEventHandler, useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import { GET_USER } from "../../redux/actionTypes/user"
+
+const Login = () => {
+  const [token, setToken] = useLocalStorage<LoginResponse | null>("token", null)
+  const [loginData, setLoginData] = useState<LoginData>({
+    email: undefined,
+    password: undefined
+  })
+  
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { mutate, isSuccess, data, isError, error } = useLoginMutate()
+  const handleNavigate = (route: string) => navigate(`/${route}`)
+
+  useEffect(() => {
+    if (token) {
+      handleNavigate("compressor")
+    } else {
+      handleNavigate("login")
+    }
+  }, [token])
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      submit()
+    }
+  }
+
+  const handleLogin: ChangeEventHandler<HTMLInputElement> = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setLoginData({ ...loginData, [name]: value })
+  }
+
+  const submit = () => {
+    mutate(loginData)
+  }
+
+  useEffect(() => {
+    if (isSuccess && data.data) {
+      // @ts-ignore:next-line
+      setToken(data.data.token)
+
+      dispatch({
+        type: GET_USER,
+        // @ts-ignore:next-line
+        payload: data.data.user,
+      })
+
+      handleNavigate('chats')
+    }
+  }, [isSuccess, data])
+
+  return (
+    <StyledLogin>
+      <LoginForm
+        // @ts-ignore:next-line
+        error={ error?.response.data.message }
+        isError={ isError }
+        submit={ submit }
+        handleLogin={ handleLogin }
+        handleNavigate={ handleNavigate }
+        handleKeyPress={ handleKeyPress }
+      />
+    </StyledLogin>
+  )
+}
+
+export default Login
