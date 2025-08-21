8294250945f90fe932642d5437dbf2927ec4a1cb
Daniel Batista <danielbped@gmail.com>
2025-08-21 11:27:36 -0300

    feat: adds error message component

---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,10 @@
+import { type ErrorMessageProps } from "../../interfaces/components.interface"
+import { StyledErrorMessage } from "./style"
+
+const ErrorMessage = (props: ErrorMessageProps) => {
+  return (
+    <StyledErrorMessage>{ props.message }</StyledErrorMessage>
+  )
+}
+
+export default ErrorMessage
